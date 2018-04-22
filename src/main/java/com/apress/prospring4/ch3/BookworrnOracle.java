228a23439a6946e11960929ad900816350124269
py4ina <py4ina@gmail.com>
2018-04-22 20:03:40 +0300

    66 st

--- src/main/java/com/apress/prospring4/ch3/BookworrnOracle.java
+++ src/main/java/com/apress/prospring4/ch3/BookworrnOracle.java
@@ -1,15 +1,15 @@
-//package com.apress.prospring4.ch3;
-//
-//public class BookworrnOracle implements Oracle {
-//
+package com.apress.prospring4.ch3;
+
+public class BookworrnOracle implements Oracle {
+
 //    private Encyclopedia encyclopedia;
 //
 //    public void setEncyclopedia(Encyclopedia encyclopedia) {
 //        this.encyclopedia = encyclopedia;
 //    }
-//
-//    @Override
-//    public String defineMeaningOfLife() {
-//        return "Encyclopedias are а waste of money - use the Internet";
-//    }
-//}
+
+    @Override
+    public String defineMeaningOfLife() {
+        return "Encyclopedias are а waste of money - use the Internet";
+    }
+}
