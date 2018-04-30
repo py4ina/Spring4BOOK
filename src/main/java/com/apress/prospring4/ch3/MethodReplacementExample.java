c208b1e969f3e3ce6fedaafb6045ae70d34e164e
py4ina <py4ina@gmail.com>
2018-04-30 22:08:54 +0300

    109 st

---
+++ src/main/java/com/apress/prospring4/ch3/MethodReplacementExample.java
@@ -0,0 +1,33 @@
+package com.apress.prospring4.ch3;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.util.StopWatch;
+
+public class MethodReplacementExample {
+    public static void main(String[] args) {
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+        ctx.load("META-INF/spring/app-context-xml.xml");
+        ctx.refresh();
+
+        ReplacementTarget replacementTarget = (ReplacementTarget) ctx.getBean("replacementTarget");
+        ReplacementTarget standardTarget = (ReplacementTarget) ctx.getBean("standardTarget");
+
+        displayInfo(replacementTarget);
+        displayInfo(standardTarget);
+    }
+
+    private static void displayInfo(ReplacementTarget target) {
+        System.out.println(target.formatMessage("Hello World!"));
+
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        for (int x = 0; x < 1_000_000; x++) {
+            String out = target.formatMessage("foo");
+        }
+
+        stopWatch.stop();
+
+        System.out.println("1_000_000 invocations took: " + stopWatch.getTotalTimeMillis() + "ms");
+    }
+}
