896cdf8132fc291ea62b385678e4dc82c328772d
py4ina <py4ina@gmail.com>
2018-05-15 21:48:11 +0300

    157 st

---
+++ src/main/java/com/apress/prospring4/ch4/AccessingFactoryBeans.java
@@ -0,0 +1,23 @@
+package com.apress.prospring4.ch4;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import java.security.MessageDigest;
+
+public class AccessingFactoryBeans {
+    public static void main(String[] args) {
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+        ctx.load("META-INF/spring/app-context-xml.xml");
+        ctx.refresh();
+
+        MessageDigest digest = (MessageDigest) ctx.getBean("shaDigest");
+        MessageDigestFactoryBean factoryBean = (MessageDigestFactoryBean) ctx.getBean("&shaDigest");
+
+        try {
+            MessageDigest shaDigest = factoryBean.getObject();
+            System.out.println(shaDigest.digest("Hello world".getBytes()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
