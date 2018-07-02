a0d4ac263cdf55b6fbcd262bf90a6fd81a5cd4cd
py4ina <py4ina@gmail.com>
2018-07-02 07:47:34 +0300

    214 st

---
+++ src/main/java/com/apress/prospring4/ch5/SecurityAdvice.java
@@ -0,0 +1,29 @@
+package com.apress.prospring4.ch5;
+
+import org.springframework.aop.MethodBeforeAdvice;
+
+import java.lang.reflect.Method;
+
+public class SecurityAdvice implements MethodBeforeAdvice {
+    private SecurityManager securityManager;
+
+    public SecurityAdvice(){
+        this.securityManager = new SecurityManager();
+    }
+
+    @Override
+    public void before(Method method, Object[] args, Object target) throws Throwable {
+        UserInfo user = securityManager.getLoggedOnUser();
+
+        if (user == null) {
+            System.out.println("No user authenticated");
+            throw new SecurityException("You must login before attempting to invoke the method: " + method.getName());
+        } else if ("chris".equals(user.getUserName())) {
+            System.out.println("Logged in user is chris - ОКАУ !");
+        } else {
+            System.out.println("Logged in user is " + user.getUserName() + " NOT GOOD :(");
+            throw new SecurityException("User " + user.getUserName() + " is not allowed access to method " + method.getName());
+        }
+
+    }
+}
---
+++ src/main/java/com/apress/prospring4/ch5/SecurityExample.java
@@ -0,0 +1,41 @@
+package com.apress.prospring4.ch5;
+
+import org.springframework.aop.framework.ProxyFactory;
+
+public class SecurityExample {
+    public static void main(String[] args) {
+        SecurityManager mgr = new SecurityManager();
+        SecureBean bean = getSecureBean();
+
+        mgr.login("chris", "pwd");
+        bean.writeSecureMessage();
+        mgr.logout();
+
+        try {
+            mgr.login("invaliduser", "pwd");
+            bean.writeSecureMessage();
+        } catch (SecurityException e) {
+            System.out.println("Exception Caught: " + e.getMessage());
+        } finally {
+            mgr.logout();
+        }
+
+        try {
+            bean.writeSecureMessage();
+        } catch (SecurityException e) {
+            System.out.println("Exception Caught: " + e.getMessage());
+        }
+    }
+
+    private static SecureBean getSecureBean() {
+        SecureBean target = new SecureBean();
+        SecurityAdvice advice = new SecurityAdvice();
+
+        ProxyFactory factory = new ProxyFactory();
+        factory.setTarget(target);
+        factory.addAdvice(advice);
+
+        SecureBean proxy = (SecureBean) factory.getProxy();
+        return proxy;
+    }
+}
