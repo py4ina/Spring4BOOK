622f64fc34979435a3d63464f4c09cc5e63ebbec
py4ina <Reper245!>
2018-07-10 11:52:06 +0300

    226 st

--- src/main/java/com/apress/prospring4/ch5/SimpleThrowsAdvice.java
+++ src/main/java/com/apress/prospring4/ch5/SimpleThrowsAdvice.java
@@ -29,11 +29,18 @@ public class SimpleThrowsAdvice implements ThrowsAdvice {
         }
     }
 
-    public void afterThrowing(Exception ex) {
+    public void afterThrowing(Exception ex) throws Throwable {
         System.out.println("***");
+        System.out.println("Generic Exception Capture");
         System.out.println("Caught: " + ex.getClass().getName());
         System.out.println("***\n");
     }
 
-    public void afterThrowing(Method method, Object[] args, Object target, IllegalArgumentException ex ){}
+    public void afterThrowing(Method method, Object[] args, Object target, IllegalArgumentException ex ) throws Throwable {
+        System.out.println("***");
+        System.out.println("IllegalArgumentException Capture");
+        System.out.println("Caught: " + ex.getClass().getName());
+        System.out.println("Method: " + method.getName());
+        System.out.println("***\n");
+    }
 }
