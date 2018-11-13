30961afbc1c038da9f850c643722526ace7dead0
Vitalik <py4ina@gmail.com>
2018-11-13 19:06:17 +0200

    364 st

---
+++ src/main/java/com/apress/prospring4/XML_SAX/SAXLocalNameCount.java
@@ -0,0 +1,86 @@
+package com.apress.prospring4.XML_SAX;
+
+import javax.xml.parsers.*;
+import org.xml.sax.*;
+import org.xml.sax.helpers.*;
+
+import java.util.*;
+import java.io.*;
+
+public class SAXLocalNameCount  extends DefaultHandler {
+
+    private Hashtable tags;
+
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+        String filename = "/home/vitalik/Documents/batches.xml";
+
+        for (int i = 0; i < args.length; i++) {
+            filename = args[i];
+            if (i != args.length - 1) {
+                usage();
+            }
+        }
+
+        if (filename == null) {
+            usage();
+        }
+
+        SAXParserFactory spf = SAXParserFactory.newInstance();
+        spf.setNamespaceAware(true);
+        SAXParser saxParser = spf.newSAXParser();
+
+        XMLReader xmlReader = saxParser.getXMLReader();
+        xmlReader.setContentHandler(new SAXLocalNameCount());
+        xmlReader.parse(convertToFileURL(filename));
+    }
+
+    public void startDocument() throws SAXException {
+        tags = new Hashtable();
+    }
+
+    public void startElement(String namespaceURI,
+                             String localName,
+                             String qName,
+                             Attributes atts)
+            throws SAXException {
+
+        String key = localName;
+        Object value = tags.get(key);
+
+        if (value == null) {
+            tags.put(key, new Integer(1));
+        }
+        else {
+            int count = ((Integer)value).intValue();
+            count++;
+            tags.put(key, new Integer(count));
+        }
+    }
+    public void endDocument() throws SAXException {
+        Enumeration e = tags.keys();
+        while (e.hasMoreElements()) {
+            String tag = (String)e.nextElement();
+            int count = ((Integer)tags.get(tag)).intValue();
+            System.out.println("Local Name \"" + tag + "\" occurs "
+                    + count + " times");
+        }
+    }
+
+    private static String convertToFileURL(String filename) {
+        String path = new File(filename).getAbsolutePath();
+        if (File.separatorChar != '/') {
+            path = path.replace(File.separatorChar, '/');
+        }
+
+        if (!path.startsWith("/")) {
+            path = "/" + path;
+        }
+        return "file:" + path;
+    }
+
+    private static void usage() {
+        System.err.println("Usage: SAXLocalNameCount <file.xml>");
+        System.err.println("       -usage or -help = this message");
+        System.exit(1);
+    }
+}
