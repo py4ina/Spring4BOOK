447833ce2ed6b00263472e76985ce26748c621cd
Vitalik <py4ina@gmail.com>
2018-12-26 12:10:13 +0200

    480 st

--- src/main/java/com/apress/prospring4/ch9/ContactServiceImpl.java
+++ src/main/java/com/apress/prospring4/ch9/ContactServiceImpl.java
@@ -1,56 +1,51 @@
 package com.apress.prospring4.ch9;
 
-import com.google.common.collect.Lists;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.TransactionCallback;
-import org.springframework.transaction.support.TransactionTemplate;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 @Service("contactService")
 @Repository
+@Transactional
 public class ContactServiceImpl implements ContactService {
-    private ContactRepository contactRepository;
-    private TransactionTemplate transactionTemplate;
+    @PersistenceContext(unitName = "emfA")
+    private EntityManager emA;
 
-    @PersistenceContext
-    private EntityManager em;
-
-    @Autowired
-    public void setContactRepository(ContactRepository contactRepository) {
-        this.contactRepository = contactRepository;
-    }
-
-    public void setTransactionTemplate(TransactionTemplate transactionTemplate) {
-        this.transactionTemplate = transactionTemplate;
-    }
+    @PersistenceContext(unitName = "emfB")
+    private EntityManager emB;
 
     @Override
+    @Transactional(readOnly = true)
     public List<Contact> findAll() {
-        return Lists.newArrayList(contactRepository.findAll());
+        return null;
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Contact findById(Long id) {
-        return contactRepository.findOne(id);
+        return null;
     }
 
     @Override
     public Contact save(Contact contact) {
-        return contactRepository.save(contact);
+        Contact contactB = new Contact();
+        contactB.setFirstName(contact.getFirstName());
+        contactB.setLastName(contact.getLastName());
+        if (contact.getId() == null) {
+            emA.persist(contact);
+            emB.persist(contactB);
+        } else {
+            emA.merge(contact);
+            emB.merge(contact);
+        }
+        return contact;
     }
 
     @Override
     public long countAll() {
-        return transactionTemplate.execute(new TransactionCallback<Long>() {
-            @Override
-            public Long doInTransaction(TransactionStatus transactionStatus) {
-                return em.createNamedQuery("Contact.countAll", Long.class).getSingleResult();
-            }
-        });
+        return 0;
     }
 }
