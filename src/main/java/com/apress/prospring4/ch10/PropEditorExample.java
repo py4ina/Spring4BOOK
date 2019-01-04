007bab66197ebc06d6913050fa1d0099634ee439
Vitalik <py4ina@gmail.com>
2019-01-04 11:53:25 +0200

    492 st

---
+++ src/main/java/com/apress/prospring4/ch10/PropEditorExample.java
@@ -0,0 +1,16 @@
+package com.apress.prospring4.ch10;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class PropEditorExample {
+    public static void main(String[] args) {
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+        ctx.load("META-INF/spring/prop-editor-app-context.xml");
+        ctx.refresh();
+
+        Contact chris = ctx.getBean("chris", Contact.class);
+        System.out.println("Chris info: " + chris);
+        Contact myContact = ctx.getBean("myContact", Contact.class);
+        System.out.println("My Contact info: " + myContact);
+    }
+}
